cdc54165e716990d54b4429135fe8411debd9fba
tima <litin.lara74mail.ru@gmail.com>
2023-07-25 17:08:46 +0300

    first

--- mydb/bin/admin.py
+++ mydb/bin/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Messanger
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'username', 'email', 'phone', 'messanger')
+    list_filter = ('messanger', 'phone', 'username', 'email')
+    search_fields = ('name', 'email', 'phone', 'username')
+    ordering = ('name', )
+
+
+@admin.register(Messanger)
+class MessangerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_published')
--- mydb/bin/models.py
+++ mydb/bin/models.py
@@ -1,28 +1,34 @@
 from django.db import models
 
 
-
 class User(models.Model):
     name = models.CharField(
         max_length=64,
-        verbose_name='name'
+        verbose_name='name',
+        blank=True
     )
     username = models.CharField(
         max_length=128,
-        verbose_name='username'
+        verbose_name='username',
+        blank=True
     )
     email = models.EmailField(
         max_length=128,
-        verbose_name='email'
+        verbose_name='email',
+        blank=True
     )
     phone = models.IntegerField(
         max_length=32,
-        verbose_name='phone'
+        verbose_name='phone',
+        blank=True,
+        null=True
     )
     messanger = models.ForeignKey(
         'Messanger',
         on_delete=models.CASCADE,
-        verbose_name='мессенджер'
+        verbose_name='мессенджеры',
+        null=False,
+        blank=False
     )
 
     class Meta:
@@ -43,6 +49,9 @@ class Messanger(models.Model):
         default=True,
         verbose_name='published'
     )
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         db_table = 'Messanger'
