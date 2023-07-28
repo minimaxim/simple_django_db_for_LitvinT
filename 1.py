c1ef9603e36bc65fe270c70cca23113d4c7e8048
tima <litin.lara74mail.ru@gmail.com>
2023-07-28 12:47:57 +0300

    second

--- 1.py
+++ 1.py
@@ -1,9 +1,8 @@
 import json
-
 import requests
 
 url = 'http://127.0.0.1:8000/api/protected/'
-token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjkwNTM3MDA0LCJpYXQiOjE2OTA1MzY3MDQsImp0aSI6IjI5NDQwMTVlYmM5ZjQ0ZGY5MTMyMGI2NjkxMTRjZTlmIiwidXNlcl9pZCI6MX0.TNtAIpsl2xiDfB8LWzv4PqX5QI-17GYpBAVTP7g1CfU'
+token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjkwNTM3NzA1LCJpYXQiOjE2OTA1Mzc0MDUsImp0aSI6ImM4ZGVhNjEzNjNjMjRlYzRiYmNiNTYzNDFiNDNjODExIiwidXNlcl9pZCI6MX0.R0lThwFUIl8dnGMazUJgRrp4LJM9h_2RjD4ilmnCAYQ'
 headers = {'Authorization': f'Bearer {token}'}
 
 response = requests.get(url, headers=headers)
@@ -11,6 +10,11 @@ response = requests.get(url, headers=headers)
 if response.status_code == 200:
     data = response.json()
     formatted_data = json.dumps(data, indent=4)
-    print(formatted_data)
+
+    # Записываем данные в файл "users_data.json"
+    with open('users_data.json', 'w') as file:
+        file.write(formatted_data)
+
+    print('Данные успешно сохранены в файл "users_data.json"')
 else:
     print('Ошибка при запросе к API:', response.status_code)
